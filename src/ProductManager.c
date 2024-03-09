bd866207b0c83cb85525ab6a1a689f2501ed3627
asfaqmoideen <asfaqmoideen@gmail.com>
2024-03-09 23:23:38 +0530

    feat: func - display main menu

---
+++ src/ProductManager.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+# include <string.h>
+# include "ProductViews.h"
+
+void showMainMenu();
+void executeMainMenu();
+void getProductDeatils();
+void addProduct();
+void updateProduct();
+void displayProducts();
+void searchProduct();
+void removeProduct();
+
+int main(){
+
+    int stopFalg = 0;
+    while (stopFalg){
+        showMainMenu();
+        // int stopFalg = executeMainMenu()
+    }
+}
