a7ba16f144f92d59546e08df8e8392796c4835ef
asfaqmoideen <asfaqmoideen@gmail.com>
2024-04-24 14:58:52 +0530

    feat; add and view products

--- src/main.c
+++ src/main.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
+
 void diplayMainMenu();
 int getOptionFromTheUser();
 int executeMainMenu(int);
 void addProduct();
+void displayProducts();
 void getProductDetails();
 
-struct product
+struct Product
 {
     int productId;
     char productName[100];
     float productPrice;
 };
 
+struct Product products [2];
+int productId = 0;
 
 int main (){
 
@@ -26,6 +30,7 @@ int main (){
       exitFlaq = executeMainMenu(option);
     }
 
+    printf("\n Exiting the application...");
     return 0;
 }
 
@@ -42,23 +47,55 @@ void diplayMainMenu(){
 
 int getOptionFromTheUser(){
     int option;
-    scanf("%d ", &option);
+    scanf("\n%d", &option);
     return option;
 }
 
 int executeMainMenu(int option){
     switch (option)
     {
+    case 1:
+        displayProducts();
     case 2:
         addProduct();
         break;
     case 0:
-        return 0;
+        return 1;
     default:
         break;
     }
+    return 0;
 }
 
 void addProduct(){
-    printf("\n Will code later");
+    productId ++;
+    char productName[100];
+    float productPrice;
+    
+    printf("Enter Product Name : ");
+    scanf("%s", productName);
+    
+    printf("Enter Product Price for %s: ", productName);
+    scanf("%f", &productPrice);
+    
+    products[productId].productId = productId;
+    strcpy( products[productId].productName, productName);
+    products[productId].productPrice = productPrice;
+
+    printf("The Product %s with Price %.2f has been successfully Added with ID : %d\n", productName, productPrice, productId);
+}
+
+void displayProducts(){
+
+    if(products == NULL){
+        printf("No Products were added yet");
+        return;
+    }
+
+    for(int i = 1; i<sizeof(products); i++){
+        printf("\n Product Id :%d",products[i].productId);
+        printf("\n Product Name : %s",products[i].productName );
+        printf("\n Product Price : %.2f", products[i].productPrice);
+    }
+
 }
